b00f91be08762ca508e589c53f56dfcf47a1e4c8
Zeyad Amin <zeyadamin291@gmail.com>
2026-09-03 09:10:19 +0300

    updating file structure

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 export async function loadNav() {
     const navBar = document.getElementById("navbar")
     try{
-        const response = await fetch('./components/navbar.html');
+        const response = await fetch('../components/navbar.html');
         console.log("fetched navbar")
         if (!(await response).ok) throw new Error('Network response has crashed');
         const html = await response.text();
@@ -15,7 +15,7 @@ export async function loadNav() {
 export async function loadFooter() {
     const footer = document.getElementsByTagName("footer")[0];
     try{
-        const response = await fetch("./components/footer.html")
+        const response = await fetch("../components/footer.html")
         if (!response.ok) throw new Error('Network response has crashed');
         const html = await response.text();
         footer.innerHTML = html;
