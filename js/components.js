d98e0d96ca219119ee9e6c09a71c6e1bdf98a73a
Zeyad Amin <zeyadamin291@gmail.com>
2026-09-05 19:04:52 +0300

    3amalt 4owayt 7agat kda

--- js/components.js
+++ js/components.js
@@ -1,26 +1,33 @@
 export async function loadNav() {
     const navBar = document.getElementById("navbar")
-    try{
+    try {
         const response = await fetch('../components/navbar.html');
         console.log("fetched navbar")
-        if (!(await response).ok) throw new Error('Network response has crashed');
+        if (!(response).ok) throw new Error('Network response has crashed');
         const html = await response.text();
         navBar.innerHTML = html;
+        const pathName = window.location.pathname.split('/').pop();
+        const navlinks = document.querySelectorAll('li a');
+        navlinks.forEach(link => {
+            if (link.getAttribute('href') === pathName) {
+                link.classList.add('active')
+            }
+        })
     }
-    catch(err){
+    catch (err) {
         console.error("Error: ", err)
     }
 }
 
 export async function loadFooter() {
     const footer = document.getElementsByTagName("footer")[0];
-    try{
+    try {
         const response = await fetch("../components/footer.html")
         if (!response.ok) throw new Error('Network response has crashed');
         const html = await response.text();
         footer.innerHTML = html;
     }
-    catch(err){
+    catch (err) {
         console.error("Error: ", err)
     }
 }
--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,64 @@
-import {loadFooter, loadNav}from './components.js'
+import { loadFooter, loadNav } from './components.js'
 loadNav()
 loadFooter()
+
+const loadProducts = async () => {
+    try {
+
+        const response = await fetch("../data/products.json")
+        const products = await response.json();
+        return products
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const laodCategories = async ()=>{
+    try {
+        const response = await fetch("../data/categories.json")
+        const categories = await response.json();
+        return categories
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+const thing = async () => {
+    let productsJson = await loadProducts();
+    let categoryJson = await laodCategories();
+    let product_list = [];
+    const parser = new DOMParser();
+
+    for (let el of productsJson) {
+        let response = await fetch('../components/products.html')
+        if (!(response).ok) throw new Error('Network response has crashed');
+        const newProductHtml = await response.text();
+        const newProduct = parser.parseFromString(newProductHtml, 'text/html');
+        newProduct.querySelector('.product img').src = el.img;
+        newProduct.querySelector('.name').innerHTML = el.name;
+        newProduct.querySelector('.price').innerHTML = el.price;
+        let productCat = newProduct.querySelector('.category')
+        for (let cat of categoryJson) {
+            if (cat.id === el.categoryId) {
+                productCat.innerHTML = cat.name;
+            }
+        }
+        product_list.push(newProduct)
+        console.log(newProduct)
+    }
+    return product_list
+}
+
+
+let product_list = document.getElementById('product-list');
+
+try {
+    
+    const products = await thing();
+    product_list.innerHTML = products.map(product => product.documentElement.outerHTML).join('');
+
+}
+catch (error) {
+    console.error(error)
+}
